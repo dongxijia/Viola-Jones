bf90f418c1d8575b10ff5a2111a0c9b3a945484b
dongxijia <32010416+dongxijia@users.noreply.github.com>
2019-10-11 11:13:38 +0800

    Update AdaBoost.py

--- violajones/AdaBoost.py
+++ violajones/AdaBoost.py
@@ -122,7 +122,7 @@ def _create_features(img_height, img_width, min_feature_width, max_feature_width
             for feature_height in range(feature_start_height, max_feature_height, feature[1]):
                 for x in range(img_width - feature_width):
                     for y in range(img_height - feature_height):
-                        features.append(HaarLikeFeature(feature, (x, y), feature_width, feature_height, 0, 1))
-                        features.append(HaarLikeFeature(feature, (x, y), feature_width, feature_height, 0, -1))
+                        #features.append(HaarLikeFeature(feature, (x, y), feature_width, feature_height, 0, 1))
+                        features.append(HaarLikeFeature(feature, (x, y), feature_width, feature_height, 0.11, -1))
     print('..done. ' + str(len(features)) + ' features created.\n')
     return features
